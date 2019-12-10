c490c4272623e7ce4e0cb4f5c1e52ef5ddddabee
taiki-yamazaki <taiki.yamazaki@okinawaopenlabs.org>
2019-12-11 02:25:09 +0900

    Add recommended list

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <Origin :address="originAddress"/>
     <Destination :address="destAddress" :arrival-time="arrivalTime"/>
     <RemainingTime :arrival-time="arrivalTime"/>
+    <RecommendedList :recommended="recommended"/>
   </div>
 </template>
 
@@ -13,12 +14,14 @@
   import Origin from './components/part/OriginPlace.vue';
   import Destination from './components/part/DestinationPlace.vue';
   import RemainingTime from './components/part/RemainingTime.vue';
+  import RecommendedList from './components/part/RecommendedList.vue';
 
   @Component({
     components: {
       Origin,
       Destination,
-      RemainingTime
+      RemainingTime,
+      RecommendedList
     },
   })
   export default class App extends Vue {
@@ -26,6 +29,13 @@
 
     private destAddress = "目的地の住所";
     private arrivalTime = this.plus6Hour(new Date());
+
+    private recommended = [
+      {name: "観光地A", category: "food"},
+      {name: "観光地B", category: "red"},
+      {name: "観光地C", category: "green"},
+      {name: "観光地D", category: "orange"},
+    ];
 
     private plus6Hour(time: Date): Date {
       const next = new Date(time.getTime());
---
+++ src/components/part/RecommendedList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="block">
+    <ul class="recommend">
+      <li v-for="item in recommended" :key="item.name">
+        <i :class="[item.category, `material-icons`]">brightness_1</i>{{item.name}}<i class="info material-icons">info</i>
+      </li>
+    </ul>
+    <div>
+      <i class="material-icons">
+        add_circle_outline
+      </i>他の候補
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from "vue-property-decorator";
+
+  export type Item = {
+    name: string;
+    category: string;
+  }
+
+  @Component
+  export default class RecommendedList extends Vue {
+    @Prop({default: () => []})
+    private recommended?: Item[];
+  }
+</script>
+
+<style scoped>
+  .block {
+    margin: 8px 8px 8px 64px;
+    border: 1px solid #1b0000;
+    border-radius: 8px;
+    padding: 8px;
+  }
+
+  .recommend {
+    list-style: none;
+  }
+
+  .recommend li {
+    padding: 2px;
+    font-size: 1.5rem;
+  }
+
+  .info {
+    float: right;
+    color: #2196f3;
+  }
+
+  .food {
+    color: #2196f3;
+  }
+
+  .red {
+    color: #f44336;
+  }
+
+  .green {
+    color: #4caf50;
+  }
+
+  .orange {
+    color: #ff5722;
+  }
+</style>
